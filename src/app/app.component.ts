710aa78bb9b5b4083a4c7f17deb6f32d8de2dc9d
Robotbino <hlongwanabino@gmail.com>
2025-10-13 20:06:02 +0200

    Changing the navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,6 +20,7 @@ export class AppComponent implements OnInit{
   ngOnInit(): void {
       this.getEmployees();
   }
+  
   public getEmployees(): void {
     this.employeeService.getEmployees().subscribe(
       (response:Employee[])=>{
