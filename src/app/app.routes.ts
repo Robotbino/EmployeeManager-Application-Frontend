d5e6b946d94b8b587944e4e14230b87008b20b0a
Robotbino <hlongwanabino@gmail.com>
2025-10-15 20:47:58 +0200

    Changing the routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
+import { RegisterComponentComponent } from './register-component/register-component.component';
 import { Routes } from '@angular/router';
+import { NavbarComponentComponent } from './navbar-component/navbar-component.component';
+import { HomeComponentComponent } from './home-component/home-component.component';
+import { LoginComponentComponent } from './login-component/login-component.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: AppComponent },
+];
